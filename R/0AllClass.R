81bdac7c9caed8d0c13eb4c9481fe2229bf48ac9
Lea Maria Musiolek <musiolel@user.hu-berlin.de>
2026-03-02 12:11:38 +0100

    Corrected slots in 0AllCLass

--- R/0AllClass.R
+++ R/0AllClass.R
@@ -116,8 +116,8 @@ setClass("WorkspaceStudio",
 #' A class extending the Login class with additional information for the IQB Studio. It can be created by the function [login_studio()].
 #'
 #' @slot login [LoginTestcenter-class]. Login information for the IQB Testcenter.
-#' @slot wsg_id ID of the workspace group the current workspace belongs to. See URL to identify the workspace group id.
-#' @slot wsg_label Label of the workspace group the current workspace belongs to.
+#' @slot ws_id ID of the workspace group the current workspace belongs to. See URL to identify the workspace group id.
+#' @slot ws_label Label of the workspace group the current workspace belongs to.
 #'
 #' @name WorkspaceTestcenter-class
 #' @rdname WorkspaceTestcenter-class
